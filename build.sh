004edd934209a3ff56f2080316a1be42a0a3099f
veethahavya-CU-cz <veethahavya@natur.cuni.cz>
2023-04-14 15:15:13 +0200

    gsl link fix in build.sh

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ while getopts ':pndh' opt; do
             echo '========================================  Compiling GWSWEX fortran module  ========================================' &> ../build.log
             gfortran -c Mtiming.f90 Mpaths.f90 Mlogger.f90 Muz.f90 Msolver.f90 Mstorages.f90 model.f90 \
                 -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+                -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
                 -fopenmp -Wall -Wno-conversion -Wno-conversion-extra -Wno-tabs -O3 -ftree-vectorize -frecursive -fPIC -march=znver2 -mtune=znver2 -ffree-line-length-1024\
                 >>../build.log 2>&1
             # -pedantic
@@ -27,7 +27,7 @@ while getopts ':pndh' opt; do
                 export NPY_DISTUTILS_APPEND_FLAGS=1
                 (f2py3 --verbose -c -m gwswex_wrapper --build-dir f2py_scratch --fcompiler=gnu95 --f90flags='-fopenmp -march=znver2 -mtune=znver2  -ftree-vectorize -frecursive' --opt='-O3' \
                     -I. -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                    -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+                    -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
                     Mtiming.o Mpaths.o Mlogger.o Muz.o Msolver.o Mstorages.o model.o GWSWEX_wrapper.f90) >>../build.log 2>&1
                     if [ $? -eq 0 ]; then
                         echo $'========================================  Successfully compiled GWSWEX python wrapper  ======================================== \n\n\n' >> ../build.log
@@ -54,7 +54,7 @@ while getopts ':pndh' opt; do
             echo '========================================  Compiling GWSWEX fortran module  ========================================' &> ../build.log
             gfortran -c Mtiming.f90 Mpaths.f90 Mlogger.f90 Muz.f90 Msolver.f90 Mstorages.f90 model.f90 \
                 -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                -L/usr/local/lib/ -lfgsl -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+                -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
                 -Wall -Wno-conversion -Wno-conversion-extra -Wno-tabs -O3 -fPIC -march=znver2 -mtune=znver2 -ffree-line-length-1024\
                  >>../build.log 2>&1
                  # -pedantic
@@ -65,7 +65,7 @@ while getopts ':pndh' opt; do
                 export NPY_DISTUTILS_APPEND_FLAGS=1
                 (f2py3 --verbose -c -m gwswex_wrapper --build-dir f2py_scratch --fcompiler=gnu95 --f90flags='-march=znver2 -mtune=znver2' --opt='-O3' \
                     -I. -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                    -L/usr/local/lib/ -lfgsl -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+                    -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
                     Mtiming.o Mpaths.o Mlogger.o Muz.o Msolver.o Mstorages.o model.o GWSWEX_wrapper.f90) >>../build.log 2>&1
                     if [ $? -eq 0 ]; then
                         echo $'========================================  Successfully compiled GWSWEX python wrapper  ======================================== \n\n\n' >> ../build.log
@@ -92,7 +92,7 @@ while getopts ':pndh' opt; do
             echo '========================================  Compiling GWSWEX fortran module  ========================================' &> ../build.log
             gfortran -c Mtiming.f90 Mpaths.f90 Mlogger.f90 Muz.f90 Msolver.f90 Mstorages.f90 model.f90 -g -fbacktrace -fcheck=all \
                 -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+                -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
                 -fopenmp -Wall -Wno-conversion -Wno-conversion-extra -Wno-tabs -O3 -fPIC -march=znver2 -mtune=znver2 -ffree-line-length-1024\
                 >>../build.log 2>&1
             # -pedantic -fsanitize=address,zero,undefined
@@ -103,7 +103,7 @@ while getopts ':pndh' opt; do
                 export NPY_DISTUTILS_APPEND_FLAGS=1
                 (gfortran Mtiming.o Mpaths.o Mlogger.o Muz.o Msolver.o Mstorages.o model.o ../testGWSWEX.f90 -o GWSWEX_debugger -g -fopenmp -march=znver2 -mtune=znver2 -O3 \
                     -I. -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-                    -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime) >>../build.log 2>&1
+                    -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime) >>../build.log 2>&1
                     if [ $? -eq 0 ]; then
                         echo $'========================================  Successfully compiled GWSWEX debugger  ======================================== \n\n\n' >> ../build.log
                         (rm -f ../libs/*.so 2>/dev/null && mv GWSWEX_debugger ../bin/ 2>/dev/null) \
@@ -136,7 +136,7 @@ if (( $OPTIND == 1 )); then
     echo '========================================  Compiling GWSWEX fortran module  ========================================' &> ../build.log
     gfortran -c Mtiming.f90 Mpaths.f90 Mlogger.f90 Muz.f90 Msolver.f90 Mstorages.f90 model.f90 \
         -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-        -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+        -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
         -fopenmp -Wall -Wno-conversion -Wno-conversion-extra -Wno-tabs -O3 -fPIC -march=znver2 -mtune=znver2 -ffree-line-length-1024\
         >>../build.log 2>&1
         # -pedantic
@@ -147,7 +147,7 @@ if (( $OPTIND == 1 )); then
         export NPY_DISTUTILS_APPEND_FLAGS=1
         (f2py3 --verbose -c -m gwswex_wrapper --build-dir f2py_scratch --fcompiler=gnu95 --f90flags='-fopenmp -march=znver2 -mtune=znver2' --opt='-O3' \
             -I. -I/usr/local/include -I/usr/local/include/yaml-fortran -I/usr/local/include/fgsl/ \
-            -L/usr/local/lib/ -lfgsl -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
+            -L/usr/local/lib/ -lfgsl -lgsl -lgslcblas -lm -lgomp -lyaml-interface -lyaml-read -lyaml-wrapper -lyaml-cpp -ldatetime \
             Mtiming.o Mpaths.o Mlogger.o Muz.o Msolver.o Mstorages.o model.o GWSWEX_wrapper.f90) >>../build.log 2>&1
             if [ $? -eq 0 ]; then
                 echo $'========================================  Successfully compiled GWSWEX python wrapper  ======================================== \n\n\n' >> ../build.log
